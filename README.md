af9ea52754440e65074d6037a3c9ba95fd5dffa6
Maxime-Coustes <maxime.coustes@hotmail.fr>
2016-11-29 15:22:22 +0100

    Depot initial

---
+++ README.md
@@ -0,0 +1,4 @@
+laloupe-1116-elevenmx
+=====================
+
+A Symfony project created on November 29, 2016, 3:13 pm.
---
+++ src/ElevenmxBundle/ElevenmxBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ElevenmxBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ElevenmxBundle extends Bundle
+{
+}
