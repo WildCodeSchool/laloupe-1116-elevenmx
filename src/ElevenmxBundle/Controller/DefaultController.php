2b84e992f786c06ca901c83cc7cbf10426796ba4
Maxime-Coustes <maxime.coustes@hotmail.fr>
2016-11-30 17:44:44 +0100

    fonctionnalité envoi mail lors de la création client

--- src/ElevenmxBundle/Controller/DefaultController.php
+++ src/ElevenmxBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace ElevenmxBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -10,4 +11,36 @@ class DefaultController extends Controller
     {
         return $this->render('ElevenmxBundle:Default:index.html.twig');
     }
+
+    public function redirectionAction()
+    {
+        return $this->render('ElevenmxBundle:Default:formMail.html.twig');
+    }
+// ******************************* envoi de mail **********************************
+    public function sendMailAction()
+    {
+
+        $Request = $this->getRequest();
+        if ($Request->getMethod() == "POST") {
+            $Subject = $Request->get("Subject");
+            $message = $Request->get("message") . " " . $Request->get("Nom") . " " . $Request->get("Prénom") . " " . $Request->get("Mail") . " " . $Request->get("Téléphone") . " " . $Request->get("Entreprise") . " " . $Request->get("Login") . " " . $Request->get("Password");
+
+
+
+            $message = \Swift_Message::newInstance('Test')
+                ->setSubject($Subject)
+                ->setFrom('javadescavernes38@gmail.com')
+                ->setTo('javadescavernes38@gmail.com')
+                ->setBody($message);
+            $this->get('mailer')->send($message);
+
+
+            return $this->render('ElevenmxBundle:Default:formMail.html.twig');
+
+        }
+        return $this->render('ElevenmxBundle:Default:formMail.html.twig');
+
+    }
+
+    // ******************************** fin d'envoi de mail ****************************************************
 }
