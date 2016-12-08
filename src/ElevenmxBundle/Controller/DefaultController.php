dccd011f61f381e83df0405cd1960ff35f61a75d
YlcDev <yannicklc.dev@gmail.com>
2016-12-08 09:12:05 +0100

    update security access

--- src/ElevenmxBundle/Controller/DefaultController.php
+++ src/ElevenmxBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace ElevenmxBundle\Controller;
 
+use ElevenmxBundle\ElevenmxBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -9,6 +10,23 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        // debut test
+        $em = $this->getDoctrine()->getManager();
+        $userActif = $this->getUser();
+        if (!$userActif){
+//            echo ('Pas connecté !');
+            return $this->redirectToRoute("fos_user_security_login");
+        } elseif ($userActif->hasRole('ROLE_SUPER_ADMIN')) {
+            echo ('Soupppppppppppeeeeeeeeeeeeeeeeeeer Admin !');
+        } elseif ($userActif->hasRole('ROLE_ADMIN')) {
+            echo ('team Admin...');
+        } elseif ($userActif->hasRole('ROLE_GRAPH')) {
+            return $this->redirectToRoute("graphiste_index");
+//            http_redirect("bundle/graphiste/index.htlm.twig");
+        } else {
+            echo ('client');
+        }
+
         return $this->render('ElevenmxBundle:Default:index.html.twig');
     }
 
@@ -43,4 +61,16 @@ class DefaultController extends Controller
     }
 
     // ******************************** fin d'envoi de mail ****************************************************
+
+
+
+    // ******************************** Debut Bloc redirection *************************************************
+
+
+
+
+    // ******************************** Fin Bloc redirection *************************************************
+
+
+
 }
