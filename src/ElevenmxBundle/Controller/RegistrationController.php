8b76a5c05f45302756cf5707a2b28fce3e16086f
Maxime-Coustes <maxime.coustes@hotmail.fr>
2017-01-11 11:07:30 +0100

    necessary to pull

---
+++ src/ElevenmxBundle/Controller/RegistrationController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 11/01/17
+ * Time: 10:11
+ */
+
+namespace ElevenmxBundle\Controller;
+
+
+class RegistrationController
+{
+
+}
---
+++ src/ElevenmxBundle/Form/RegistrationForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 11/01/17
+ * Time: 10:40
+ */
+
+namespace ElevenmxBundle\Form;
+
+
+class RegistrationForm
+{
+
+}
