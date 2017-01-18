325873c0ba6ad09c104b5a16d4192cec6c306dcc
Maxime-Coustes <maxime.coustes@hotmail.fr>
2017-01-18 18:19:04 +0100

    recherche impeccable

--- src/ElevenmxBundle/Resources/public/js/recherchejQuery.js
+++ src/ElevenmxBundle/Resources/public/js/recherchejQuery.js
@@ -6,24 +6,25 @@ $("#search").keyup(function() // A chaque levée d'une touche du clavier sur l'i
 
 {
 
-    console.log('On a appuyé sur une touche, je m"active.');
+
+    // Nouvelle regex qui comprend la valeur de l'input 'i'
+
+    var myRegExp = new RegExp($(this).val(), 'gi');
 
 
-    // Nouvelle regex qui comprend la valeur de l'input
 
-    var myRegExp = new RegExp($(this).val(), 'i');
-
-    $('.Filtre').each(function(){ // Pour chaque élément qui a la classe citée, faire :
+    $('.filtre').each(function(){ // Pour chaque élément qui a la classe citée, faire :  et $(this) --> "celui d'avant"
 
 // Si la vérification de la regex renvoie au minimum quelque chose, faire :
-        console.log($(this).parent().parent().parent().parent());
-        if (($(this).html().match(myRegExp) || []).length != 0)
+
+
+        if ($(this).html().match(myRegExp) != null)
 
         {
 
             // On affiche le bloc correspondant avec un effet slide
 
-            $(this).parent().parent().parent().parent().show('slide');
+            $(this).show('slide');
 
         }
 
@@ -33,9 +34,12 @@ $("#search").keyup(function() // A chaque levée d'une touche du clavier sur l'i
 
             // On masque le bloc correspondant avec un effet slideOut
 
-            $(this).parent().parent().parent().parent().hide('slideOut');
+            $(this).hide('slideOut');
 
         }
 
     });
+
+
+
 });
