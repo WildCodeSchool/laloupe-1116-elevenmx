8c732e24db071a3bc05f5092039e9411a38b033d
Maxime-Coustes <maxime.coustes@hotmail.fr>
2017-02-01 09:15:04 +0100

    necessary to pull

--- src/ElevenmxBundle/Controller/ProjetController.php
+++ src/ElevenmxBundle/Controller/ProjetController.php
@@ -122,7 +122,6 @@ class ProjetController extends Controller
             'projet' => $projet,
             'form' => $form->createView(),
             'gestionstatus' => $gestionstatus,
-            'user' => $user
         ));
     }
 
